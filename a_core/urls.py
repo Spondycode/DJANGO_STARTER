e0f9f49445502f900d0562871140f7b6dfdf3938
David Allen <spondicious@me.com>
2024-03-12 13:32:45 +0100

    Add profile view and URLs for user profiles

--- a_core/urls.py
+++ a_core/urls.py
@@ -5,11 +5,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from a_users.views import profile_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('', include('a_home.urls')),
     path('profile/', include('a_users.urls')),
+    path('@<username>/', profile_view, name="profile"),
 ]
 
 # Only used in Development
--- a_users/forms.py
+++ a_users/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from django import forms
+from django.contrib.auth.models import User
 from .models import Profile
 
 class ProfileForm(ModelForm):
@@ -11,3 +12,11 @@ class ProfileForm(ModelForm):
             "displayname" : forms.TextInput(attrs={"placeholder": "Add Displayname"}),
             "info" : forms.Textarea(attrs={"rows":3, "placeholder": "Add Information about you..."}),
         }
+        
+        
+class EmailForm(ModelForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ("email",)
